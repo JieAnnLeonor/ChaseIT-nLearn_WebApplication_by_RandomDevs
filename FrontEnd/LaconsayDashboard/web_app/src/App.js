cc87e0109f8d880c241c79cbbb1eed67eb1b700a
Raphael Laconsay <raphaelpierreantonni.laconsay@cit.edu>
2022-12-21 23:59:08 +0800

    Update App.js

--- FrontEnd/LaconsayDashboard/web_app/src/App.js
+++ FrontEnd/LaconsayDashboard/web_app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Navbar from './components/NAVBAR';
 
 function App() {
   return (
     <div className="App">
-      <div className="NAVBAR">NAVBAR</div> 
+      <Navbar />
       <Dashboard />
     </div>        
   );
