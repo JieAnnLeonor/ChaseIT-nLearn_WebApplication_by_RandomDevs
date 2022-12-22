90513367cadcb1e7fdf805908621f36ff4d6454c
Jie Ann Leonor <jieann.leonor@cit.edu>
2022-12-22 12:26:21 +0800

    Add Lessons

--- FrontEnd/chaseitnlearn_webapp/src/App.js
+++ FrontEnd/chaseitnlearn_webapp/src/App.js
@@ -9,6 +9,9 @@ import SignUp from "./components/frontpage/routes/SignUp";
 import Register from "./components/frontpage/routes/Register"
 import StudentDashboard from "./components/dashboard/dashpages/StudentDashboard";
 import MyCourses from "./pages/MyCourses";
+import EnrollCourse from "./pages/EnrollCourse";
+import TakeLessons from "./pages/TakeLessons";
+import Chapter1 from "./routes/Chapter1";
 
 function App() {
   return (
@@ -22,6 +25,9 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/studentdashboard" element={<StudentDashboard/>}/>      
         <Route path="/readallcourses" element={<MyCourses />} />
+        <Route path="/enrollcourse" element={<EnrollCourse />} />
+        <Route path="/takelessons" element={<TakeLessons />} />       
+        <Route path="/lesson1/chapter1" element={<Chapter1/>}/>
       </Routes>
     </div>
   );
