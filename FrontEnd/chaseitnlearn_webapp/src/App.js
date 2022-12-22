3ddd17dd1ab9c5d5e4aa2fc8c37353699fcf38c1
Jaymark Carba <jaymark.carba@cit.edu>
2022-12-22 14:21:51 +0800

    Add read resources

--- FrontEnd/chaseitnlearn_webapp/src/App.js
+++ FrontEnd/chaseitnlearn_webapp/src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import CreateCourse from './services/CreateCourse';
 import CreateResources from './services/CreateResources';
-import FooterC from './services/FooterC';
+import ReadResources from './services/ReadResources';
 function App() {
   return (
     <>
         <Routes>
           <Route path="/addResources" element={<CreateResources/>} />
           <Route path="/addCourse" element={<CreateCourse/>} />
+          <Route path="/readResources" element={<ReadResources/>} />
         </Routes>
     </>
   );
