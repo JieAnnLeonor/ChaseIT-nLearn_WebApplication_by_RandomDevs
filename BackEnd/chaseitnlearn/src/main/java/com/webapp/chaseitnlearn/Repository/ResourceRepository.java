726ee9fa5f45a99f89daaf247f0ea7d043599f76
Jaymark Carba <jaymark.carba@cit.edu>
2022-12-22 18:44:33 +0800

    Add Repository resources

--- BackEnd/chaseitnlearn/src/main/java/com/webapp/chaseitnlearn/Repository/ResourceRepository.java
+++ BackEnd/chaseitnlearn/src/main/java/com/webapp/chaseitnlearn/Repository/ResourceRepository.java
@@ -1,5 +1,12 @@
 package com.webapp.chaseitnlearn.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ResourceRepository {
+import com.webapp.chaseitnlearn.Controller.ResourceEntity;
+
+
+
+@Repository
+public interface ResourceRepository extends JpaRepository<ResourceEntity, Integer>{
 
 }
