968c46e353f6a1573c50dad4edfdb0e18d1ce829
Timothy Taboada <timothyjanssen.taboada@cit.edu>
2022-12-22 15:53:27 +0800

    Stylize CreateQuiz

--- FrontEnd/chaseitnlearn_webapp/src/pages/CreateQuiz.js
+++ FrontEnd/chaseitnlearn_webapp/src/pages/CreateQuiz.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {useNavigate} from "react-router-dom"
+import { Link } from "react-router-dom";
 import TeacherNavbar from '../components/dashboard/dashpages/TeacherNavbar';
-import '../designs/CreateQuiz.css';
+import '../pagedesigns/CreateQuiz.css';
 
 /*
 const quizForm = document.getElementById('quiz-form');
@@ -27,27 +28,40 @@ function CreateQuiz(){
         </Link>  
         </div> 
 
-          <form id="quiz-form">
-            <label for="quiz-title">Quiz Title:</label><br/>
-              <input type="text" id="quiz-title" name="quiz-title"/>
+        <div className="quiz-container">
+          <form>
+            <div className="quiz-title">
+              <label className="title">Quiz Title:</label><br/>
+              <input type="text" id="title-input" className="quiz-title"/>
               <br></br>
+            </div>
+            
             <div id="questions">
-              <div class="question">
-                <label for="question-1">Question 1:</label><br/>
+              <div className="question">
+              <label for="question-1">Question 1:</label><br/>
                   <input type="text" id="question-1" name="question-1"/>
                   <br></br>
+              </div>
+                <div className="answer">
                 <label for="answer-1-1">Answer 1:</label><br/>
                   <input type="text" id="answer-1-1" name="answer-1-1"/>
                   <br></br>
+                </div>
+                <div className="answer">
                 <label for="answer-1-2">Answer 2:</label><br/>
                   <input type="text" id="answer-1-2" name="answer-1-2"/>
                   <br></br>
+                </div>
+                <div className="answer">
                 <label for="answer-1-3">Answer 3:</label><br/>
                   <input type="text" id="answer-1-3" name="answer-1-3"/>
                   <br></br>
+                </div>
+                <div className="answer">
                 <label for="answer-1-4">Answer 4:</label><br/>
                   <input type="text" id="answer-1-4" name="answer-1-4"/>
                   <br></br>
+                </div>
                 <label for="correct-answer-1">Correct Answer:</label><br/>
                   <select id="correct-answer-1" name="correct-answer-1">
                     <option value="1">Answer 1</option>
@@ -55,11 +69,12 @@ function CreateQuiz(){
                     <option value="3">Answer 3</option>
                     <option value="4">Answer 4</option>
                   </select>
-              </div>
+
             </div>
           <button type="button" id="add-question">Add Question</button>
           <button type="submit">Create Quiz</button>
-        </form>
+          </form>
+        </div>
       </>
     );
 }
