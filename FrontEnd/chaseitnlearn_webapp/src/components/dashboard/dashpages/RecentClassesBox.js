9a9aee8a9867787dd2d723bfc72a33d7104cb59d
Jie Ann Leonor <jieann.leonor@cit.edu>
2022-12-22 22:36:33 +0800

    Modify url

--- FrontEnd/chaseitnlearn_webapp/src/components/dashboard/dashpages/RecentClassesBox.js
+++ FrontEnd/chaseitnlearn_webapp/src/components/dashboard/dashpages/RecentClassesBox.js
@@ -5,7 +5,7 @@ class RecentClassesBox extends Component {
     render() {
         return(
             <>
-            <a href='/takelesson' className="item-classes" boxcolor = {this.props.boxcolor}>
+            <a href='/takelessons' className="item-classes" boxcolor = {this.props.boxcolor}>
                 <h2>{this.props.title}</h2>
                 <hr></hr>
                 <h3>Lessons: {this.props.lessons}</h3>
