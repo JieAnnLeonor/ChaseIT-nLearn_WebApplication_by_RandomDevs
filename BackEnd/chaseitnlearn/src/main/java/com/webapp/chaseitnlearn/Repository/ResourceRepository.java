dd729f5476656189609b6c6840c5243fe07b0c79
Jaymark Carba <jaymark.carba@cit.edu>
2022-12-22 18:59:18 +0800

    Fix repository

--- BackEnd/chaseitnlearn/src/main/java/com/webapp/chaseitnlearn/Repository/ResourceRepository.java
+++ BackEnd/chaseitnlearn/src/main/java/com/webapp/chaseitnlearn/Repository/ResourceRepository.java
@@ -1,10 +1,12 @@
 package com.webapp.chaseitnlearn.Repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.webapp.chaseitnlearn.Controller.ResourceEntity;
+import com.webapp.chaseitnlearn.Entity.ResourceEntity;
+
 
 @Repository
 public interface ResourceRepository extends JpaRepository<ResourceEntity, Integer>{
-
+	
 }
