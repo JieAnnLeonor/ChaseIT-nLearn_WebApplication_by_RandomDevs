77a6f4d7461593a2c869f8063664d72b4567244a
Jaymark Carba <jaymark.carba@cit.edu>
2022-12-22 18:02:57 +0800

    Invalid argument delete

--- FrontEnd/chaseitnlearn_webapp/src/pages/TeacherPage/CreateCourse.jsx
+++ FrontEnd/chaseitnlearn_webapp/src/pages/TeacherPage/CreateCourse.jsx
@@ -8,7 +8,6 @@ import Button from '@mui/material/Button';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import FooterC from './FooterC';
 import { Box } from '@mui/system';
-import BasicModal from './Modal';
 
 class CreateCourse extends Component {
     constructor(props){
@@ -70,7 +69,6 @@ class CreateCourse extends Component {
     render() {
         return (
             <>
-            <Button onClick={<BasicModal/>}>CLICK ME!</Button>
             <Box sx={{display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
                 <Box sx={{padding: 1, fontSize: "3rem", fontFamily: 'Trocchi', fontWeight: "bold"
                     ,letterSpacing: "-1px", lineHeight: "2", textAlign: "center", color: "rgb(130, 170, 227)"}}>
